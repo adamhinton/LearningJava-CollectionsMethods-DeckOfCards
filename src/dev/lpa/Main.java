83fed4e210d5dc47c9b3c2bf7c6dc9d4bb0cdafb
adamd <adam.d.hinton@gmail.com>
2024-03-17 12:09:12 -0400

    learning about Collections utils

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,5 +1,8 @@
 package dev.lpa;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -7,6 +10,32 @@ public class Main {
 
         List<Card> deck = Card.getStandardDeck();
         Card.printDeck(deck);
+
+        Card[] cardArray = new Card[13];
+        Card aceOfHearts = Card.getFaceCard(Card.Suit.HEART, 'A');
+
+        Arrays.fill(cardArray, aceOfHearts);
+
+        Card.printDeck(Arrays.asList(cardArray), "Aces of Hearts", 1);
+
+
+        // Doesn't populate with null refs
+        List<Card> cards = new ArrayList<>(52);
+        Collections.fill(cards, aceOfHearts);
+        System.out.println(cards);
+        System.out.println("size:" + cards.size());
+
+        List<Card> acesOfHearts = Collections.nCopies(13, aceOfHearts);
+        Card.printDeck(acesOfHearts, "Aces of Hearts", 1);
+
+        Card kClubs = Card.getFaceCard(Card.Suit.CLUB, 'K');
+        List<Card> ksOfClubs = Collections.nCopies(13, kClubs);
+        Card.printDeck(ksOfClubs, "Kings of Clubs", 1);
+
+//        Collections.addAll(cards, cardArray);
+//        Card.printDeck(cards, "Card collection with Aces added", 1);
+
+
 
     }
 }
